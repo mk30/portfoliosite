328b1400502ea78945fcad224471f94b0bdb6b56
Marina Kukso <marina.kukso@gmail.com>
2015-03-30 20:04:52 -0700

    init

---
+++ server.js
@@ -0,0 +1,35 @@
+var h = require('virtual-dom/h');
+var str = require('virtual-dom-stringify');
+var http = require('http');
+var fs = require('fs');
+var ecstatic = require('ecstatic');
+var st = ecstatic(__dirname);
+var hyperstream = require('hyperstream');
+var path = require('path');
+ 
+var server = http.createServer(function (req, res) {
+    if (req.url === '/') {
+        fs.readdir(__dirname + '/images', onfiles);
+    }
+    else st(req, res);
+    
+    function onfiles (err, files) {
+        var tree = h('div#content', [
+            h('h1', 'wow hello'),
+            h('div.gallery', files.map(function (x) {
+                return h('div', [
+                    h('img', {
+                        src: 'images/' + x,
+                        height: 128
+                    })
+                ]);
+            }))
+        ]);
+        fs.createReadStream(path.join(__dirname, 'index.html'))
+            .pipe(hyperstream({ '#content': str(tree) }))
+            .pipe(res)
+        ;
+        res.end(str(tree));
+    }
+});
+server.listen(6004);
