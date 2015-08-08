9cd5ee11f819eb3e792adf4697c91b469b04adc9
Marina Kukso <marina.kukso@gmail.com>
2015-08-08 12:25:43 -0700

    thumbs script makes large and small versions

--- images/thumb.sh
+++ images/thumb.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-for file in *.[jJ][pP][gG] 
-do
-  convert -resize 25% "$file" lg_"$file"
+for file in `find ./full -iname \*.jpg`; do
+  if test \! -f images/lg_`basename $file`; then
+  convert -resize 25% "$file" lg_"`basename $file`"
+  fi
+  if test \! -f images/thumbs/t_`basename $file`; then
+  convert -resize 12% "$file" thumbs/t_"`basename $file`"
+  fi
 done
