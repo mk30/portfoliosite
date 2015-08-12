bca78f5b011e859e59c1813a3bba1e8c6f5c2b67
Marina Kukso <marina.kukso@gmail.com>
2015-08-12 16:07:31 -0700

    got links from main page working correctly - still need to fix css and resize script

--- router.js
+++ router.js
@@ -4,10 +4,9 @@ router.addRoute('/', function (m){
     return h('div#content', [
         h('div.gallery', m.state.files.map(function (x) {
             return h('div', [ 
-                h('a', { 'href': '/images/thumbs/' + x }, [
+                h('a', { 'href': '/images/' + x }, [
                     h('img', {
-                        src: 'images/' + x,
-                        width: 500
+                        src: 'images/thumbs/t_' + x
                     })
                 ]) 
             ])
