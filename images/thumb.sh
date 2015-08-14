a6b49164f04506b1d3cb80b0351b070bdfc01f5e
Marina Kukso <marina.kukso@gmail.com>
2015-08-13 18:52:19 -0700

    fixed resize script

--- images/thumb.sh
+++ images/thumb.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+for file in `find ./full -iname \*.jpg`; do
+  rename '$_=lc' "$file"
+done
 
 for file in `find ./full -iname \*.jpg`; do
-  if test \! -f images/lg_`basename $file`; then
-  convert -resize 25% "$file" lg_"`basename $file`"
+  if test \! -f images/`basename $file`; then
+  convert -resize 25% "$file" "`basename $file`"
   fi
   if test \! -f images/thumbs/t_`basename $file`; then
   convert -resize 12% "$file" thumbs/t_"`basename $file`"
