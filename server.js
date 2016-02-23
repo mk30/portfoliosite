95a06e1a4ee0d093d7bca0b02b7ac8dc454fdfbe
Marina Kukso <marina.kukso@gmail.com>
2016-02-23 12:17:38 -0800

    cleaning up code - no changes to actual code

--- server.js
+++ server.js
@@ -13,20 +13,19 @@ process.setgid(process.argv[3]);
 process.setuid(process.argv[2]);
 
 var server = http.createServer(function (req, res) {
-    var m = router.match(req.url)
-    if (m) {
-        fs.readdir(__dirname + '/public/images/large',
-        function (err, files) {
-            m.state = {files : files}
-            var tree = m.fn(m);
-            var html = req.url.split('/')[1] === 'gallery'
-                ? '/public/lightbox.html' : '/public/index.html'
-            fs.createReadStream(path.join(__dirname, html))
-                .pipe(hyperstream({ '#content': str(tree) }))
-                .pipe(res)
-        });
-        
-    }
-    else st(req, res);
+  var m = router.match(req.url)
+  if (m) {
+    fs.readdir(__dirname + '/public/images/large',
+    function (err, files) {
+      m.state = {files : files}
+      var tree = m.fn(m);
+      var html = req.url.split('/')[1] === 'gallery'
+          ? '/public/lightbox.html' : '/public/index.html'
+      fs.createReadStream(path.join(__dirname, html))
+        .pipe(hyperstream({ '#content': str(tree) }))
+        .pipe(res)
+    });
+  }
+  else st(req, res);
 });
 server.listen({ fd: fd });
