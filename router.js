3d407e7b4f5b38003c5225fe4cd9875623715cc2
Marina Kukso <marina.kukso@gmail.com>
2016-03-28 15:59:16 -0700

    first go at moving all routing logic to router. also moved fs module from server to router. in process on other things - this doesn't work yet.

--- router.js
+++ router.js
@@ -1,20 +1,49 @@
+var fs = require('fs');
 var h = require('virtual-dom/h');
 var router = require('routes')();
-router.addRoute('/gall', function (m){
-  return h('div#stage', [
-    h('div.gallery', 
-      m.state.files.map(function (x) {
-        return h('div', [ 
-          h('a', { 'href': '/gallery/' + x }, [
-            h('img', {
-              src: '/public/images/thumbs/t_' + x
-            })
-          ]) 
-        ])
-      })
-    )
-  ])
+router.addRoute('/gall', function (m, req, res){
+  fs.readdir(__dirname + '/public/images/large',
+    function (err, files) {
+      var html = '/public/index.html'
+      var tree = h('div#stage', [
+        h('div.gallery', 
+          files.map(function (x) {
+            return h('div', [ 
+              h('a', { 'href': '/gallery/' + x }, [
+                h('img', {
+                  src: '/public/images/thumbs/t_' + x
+                })
+              ]) 
+            ])
+          })
+        )
+      ])
+      fs.createReadStream(path.join(__dirname, html))
+        .pipe(hyperstream({ '#content': str(tree) }))
+        .pipe(res)
+    })
 })
+router.addRoute('/gallery/:title', function (m, req, res){
+  fs.readdir(__dirname + '/public/images/large',
+    function (err, files) {
+      var html = '/public/lightbox.html'
+      var tree = h('div#stage', [
+        h('div.gallery', 
+          files.map(function (x) {
+            return h('div.players', [ 
+              h('img', {
+                src: '/public/images/large/' + x
+              })
+            ])
+          })
+        )
+      ])
+      fs.createReadStream(path.join(__dirname, html))
+        .pipe(hyperstream({ '#content': str(tree) }))
+        .pipe(res)
+    })
+})
+/*
 router.addRoute('/gallery/:title', function (m){
   return h('div#stage', m.state.files.map(function (x) {
     return h('div.players', [ 
@@ -24,6 +53,7 @@ router.addRoute('/gallery/:title', function (m){
     ])
   }))
 })
+*/
 router.addRoute('/dig', function (m){
   return h('div#stage', [
     h('div.gallery', 
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var str = require('virtual-dom-stringify');
 var http = require('http');
-var fs = require('fs');
 var ecstatic = require('ecstatic');
 var st = ecstatic(__dirname);
 var hyperstream = require('hyperstream');
@@ -13,7 +12,12 @@ process.setgid(process.argv[3]);
 process.setuid(process.argv[2]);
 
 var server = http.createServer(function (req, res) {
+  
   var m = router.match(req.url)
+  if (m) {m.fn(m, req, res)}
+  //if (m) {m.fn(m, req, res)}
+  //move the rest into individual routes
+  /*
   if (m) {
     fs.readdir(__dirname + '/public/images/large',
     function (err, files) {
@@ -27,11 +31,16 @@ var server = http.createServer(function (req, res) {
       else if (req.url.split('/')[1] === 'digital')
         {html = '/public/lightbox.html'}
       else {html = '/public/index.html'}
+      //copy all below into each route. html should be
+      //hardcoded with the html file for that route. tree
+      //will turn into the return statement that sets up the
+      //dom tree for that route
       fs.createReadStream(path.join(__dirname, html))
         .pipe(hyperstream({ '#content': str(tree) }))
         .pipe(res)
     });
   }
+  */
   else st(req, res);
 });
 server.listen({ fd: fd });
