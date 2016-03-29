98976eb5474bf672b3bd5f6abe5b28a4e56316f6
Marina Kukso <marina.kukso@gmail.com>
2016-03-28 19:35:39 -0700

    got lightbox working with new route

--- router.js
+++ router.js
@@ -1,5 +1,8 @@
 var fs = require('fs');
+var hyperstream = require('hyperstream');
+var path = require('path');
 var h = require('virtual-dom/h');
+var str = require('virtual-dom-stringify');
 var router = require('routes')();
 router.addRoute('/gall', function (m, req, res){
   fs.readdir(__dirname + '/public/images/large',
@@ -27,17 +30,15 @@ router.addRoute('/gallery/:title', function (m, req, res){
   fs.readdir(__dirname + '/public/images/large',
     function (err, files) {
       var html = '/public/lightbox.html'
-      var tree = h('div#stage', [
-        h('div.gallery', 
-          files.map(function (x) {
-            return h('div.players', [ 
-              h('img', {
-                src: '/public/images/large/' + x
-              })
-            ])
-          })
-        )
-      ])
+      var tree = h('div#stage', 
+        files.map(function (x) {
+          return h('div.players', [ 
+            h('img', {
+              src: '/public/images/large/' + x
+            })
+          ])
+        })
+      )
       fs.createReadStream(path.join(__dirname, html))
         .pipe(hyperstream({ '#content': str(tree) }))
         .pipe(res)
