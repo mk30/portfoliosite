52b497f13ec5f97cca6907e76d5335ebe02612b4
Marina Kukso <marina.kukso@gmail.com>
2016-04-22 20:43:28 -0700

    now serving static content from dirname + /public

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var str = require('virtual-dom-stringify');
 var http = require('http');
 var ecstatic = require('ecstatic');
-var st = ecstatic(__dirname);
+var st = ecstatic(__dirname + '/public');
 var hyperstream = require('hyperstream');
 var path = require('path');
 var router = require('./router.js');
@@ -15,32 +15,6 @@ var server = http.createServer(function (req, res) {
   
   var m = router.match(req.url)
   if (m) {m.fn(m, req, res)}
-  //if (m) {m.fn(m, req, res)}
-  //move the rest into individual routes
-  /*
-  if (m) {
-    fs.readdir(__dirname + '/public/images/large',
-    function (err, files) {
-      m.state = {files : files}
-      var tree = m.fn(m);
-      var html;
-      if (req.url.split('/')[1] === 'gall')
-        {html = '/public/index.html'} 
-      else if (req.url.split('/')[1] === 'gallery')
-        {html = '/public/lightbox.html'} 
-      else if (req.url.split('/')[1] === 'digital')
-        {html = '/public/lightbox.html'}
-      else {html = '/public/index.html'}
-      //copy all below into each route. html should be
-      //hardcoded with the html file for that route. tree
-      //will turn into the return statement that sets up the
-      //dom tree for that route
-      fs.createReadStream(path.join(__dirname, html))
-        .pipe(hyperstream({ '#content': str(tree) }))
-        .pipe(res)
-    });
-  }
-  */
   else st(req, res);
 });
 server.listen({ fd: fd });
